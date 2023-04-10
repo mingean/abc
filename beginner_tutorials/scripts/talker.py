aa9337d9138a93876ce69c68ad8a08052bf077c4
mingean <mingean0604@kyonggi.ac.kr>
2023-04-10 13:21:03 +0900

    ddd

--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -30,7 +30,7 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
+#jjjjjjjjj
 # Revision $Id$
 
 ## Simple talker demo that published std_msgs/Strings messages
