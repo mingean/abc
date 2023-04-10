360c9310ff8616bd38ec0f14419311459bf69096
mingean <mingean0604@kyonggi.ac.kr>
2023-04-10 13:54:20 +0900

    bc

--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -3,7 +3,7 @@
 #
 # Copyright (c) 2008, Willow Garage, Inc.
 # All rights reserved.
-#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
